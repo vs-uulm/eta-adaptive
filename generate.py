fab66c0ae695763c13d2574c6172e5594fc3d07d
ketzu <david.moedinger@ketzu.net>
2020-10-09 12:01:24 +0200

    file used to generate data

---
+++ generate.py
@@ -0,0 +1,49 @@
+from statistics import mean
+from igraph import Graph
+from scipy.stats import norm
+import numpy as np
+
+
+def frequencies(nodes, edges):
+    g = Graph.Establishment(nodes, edges, [1], [[1]])
+    spl = g.shortest_paths_dijkstra()
+    return [sum([ll.count(_) for ll in spl])/(nodes*nodes) for _ in range(max(map(max, spl)))]
+
+# Number of runs per parameter combination
+runs = range(100)
+
+# network sizes to generate
+noderange = list(range(50, 10001, 50))
+
+# number of edges each node generates when being added to the network
+edgerange = range(2, 25, 2)
+
+alldata = []
+
+try:
+    for nodes in noderange:
+        for edges in edgerange:
+            if edges >= nodes:
+                break
+            ms = []
+            ss = []
+            for run in runs:
+                freq = frequencies(nodes, edges)
+                x = list(range(len(freq)))
+
+                # use fixed size frequency representation
+                expandeddata = np.repeat(x, np.array([int(_ * 10000) for _ in freq]).astype(int))
+
+                m, s = norm.fit(expandeddata)
+                ms.append(m)
+                ss.append(s)
+            alldata.append((nodes, param, ms, ss))
+            # progress indication
+            print(nodes, param, mean(ms), mean(ss))
+except KeyboardInterrupt:
+    # allows storing of results after interrupt through ctrl+c
+    pass
+    
+from datetime import datetime
+import pickle
+pickle.dump(alldata, open("normal_approx_"+datetime.now().strftime("%Y-%m-%d-%H_%M_%S")+".p", "wb"))
